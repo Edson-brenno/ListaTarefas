793ac73b768a932459d391096f723f05a7dcc7f6
brenno <brennorebeiro@gmail.com>
2023-11-28 20:33:29 -0300

    Add retorno mensagens de error

--- apps/registrar_usuario/views.py
+++ apps/registrar_usuario/views.py
@@ -4,17 +4,28 @@ from django.contrib.auth.models import User
 def registro_usuario(request):
 
     if request.method == 'POST':
-        
+
+        # Variaveis fomulario 
         usuario = request.POST.get('username',None)
         senha = request.POST.get('senha',None)
+            
+        # Verica se o usuário existe
+        if User.objects.filter(username = usuario).exists():
 
-        if usuario != None and senha != None:
+            mensagem_error = "usuario já existe"
+
+            return render(request,'registro/registrar.html', context={"mensagem":mensagem_error})
+            
+        else:
 
             user = User.objects.create_user(username=usuario, password=senha)
 
             user.save()
+
+            mensagem_error = "Usuario Criado"
             
-        return render(request,'registro/registrar.html', context={})
+            return redirect('/login/')
+    
     else:
         
-        return render(request,'registro/registrar.html', context={})
+        return render(request,'registro/registrar.html', context={"mensagem":'O Usuario e senha não podem ser nulo'})
