479177e62273be65645af23dd59e75173627a6cd
brenno <brennorebeiro@gmail.com>
2023-11-28 00:35:31 -0300

    Add view registro usuario

--- apps/registrar_usuario/views.py
+++ apps/registrar_usuario/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 def registro_usuario(request):
 
-    return HttpResponse('<h1>Cadastro</h1>')
+    if request.method == 'POST':
+        
+        usuario = request.POST.get('username',None)
+        senha = request.POST.get('senha',None)
+
+        if usuario != None and senha != None:
+
+            user = User.objects.create_user(username=usuario, password=senha)
+
+            user.save()
+            
+        return render(request,'registro/registrar.html', context={})
+    else:
+        
+        return render(request,'registro/registrar.html', context={})
