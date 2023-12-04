87fdc29d30b6ae1f3b1f14f5be88cae3846e133d
edson brenno <brennorebeiro@gmail.com>
2023-12-03 23:12:19 -0300

    Add login e logout

--- apps/login/urls.py
+++ apps/login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.login.views import login
+from apps.login.views import login,logout
 
 urlpatterns = [
     path('login/', login),
+    path('logout/',logout)
 ]
--- apps/login/views.py
+++ apps/login/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 # Create your views here.
 
 def login(request):
+
+    mensagem = ""
+
+    if request.method == 'POST':
+
+        usuario_form = request.POST.get('usuario','') 
+        senha_form = request.POST.get('senha','')
+        
+        # Verifica a autenticidade do usuário
+        usuario = authenticate(request, username=usuario_form, password=senha_form)
+
+        if usuario is not None:
+            
+            auth_login(request, usuario)
+
+            return redirect('/index/')
+        else:
+            mensagem = ("usuario ou senha inválidos")
     
-    return render(request, 'login/login.html', context={})
+    
+    return render(request, 'login/login.html', context={'mensagem':mensagem})
+   
+
+def logout(request):
+
+    auth_logout(request)
+
+    return redirect('/login/')
