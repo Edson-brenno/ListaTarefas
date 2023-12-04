69280d7c623e58c68399a2d59c1d360bb718fc56
edson brenno <brennorebeiro@gmail.com>
2023-12-04 00:17:53 -0300

    update models

--- apps/index/models.py
+++ apps/index/models.py
@@ -7,6 +7,7 @@ class TbTarefasFat(models.Model):
 
     id = models.BigAutoField(primary_key=True)
     titulo = models.CharField(max_length=150, blank=False, null=False)
+    data_atividade = models.DateField(blank=False, null=False)
     data_criacao = models.DateField(auto_now_add=True, blank=False, null=False)
     foi_finalizada = models.BooleanField(default=False)
     foi_arquivada = models.BooleanField(default=False)
